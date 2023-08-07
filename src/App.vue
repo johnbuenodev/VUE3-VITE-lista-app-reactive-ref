4b2ebde99d445cbd9121f6b47adb6fb023f2884a
johnbuenodev <johnbuenodev@gmail.com>
2023-08-06 21:02:34 -0300

    Lista de objeto reactive e variavel ref, iniciando estudo sobre COMPOSITION API

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<script setup>
+
+import { ref, reactive } from 'vue';
+
+//com vite vue - composition api precisa criar um ref 
+//Para quye quando ocorra uma alteração em uma variavel sejá reativo
+//e em seguida já seja renderizado novamente o valor
+// com projeto normal de vue que é o options api ele já é reativo
+
+//usado para tipos primitivos string, NUMEROS ... 
+let count = ref(0);
+//quando criar uma variavel com ref 
+//fica dentro de value 
+//EXEMPLO count.value = receber o valor aqui
+
+
+//reactive É UTILIZADO PARA objetos
+
+let courses = reactive([
+  {
+   title: "Javascript"
+  },
+  {
+   title: "Angular"
+  },
+  {
+   title: "flutter"
+  },
+  {
+   title: "React native"
+  },
+  {
+   title: "Ionic"
+  }
+]);
+
+let newCourse = reactive({
+  'title': ''
+});
+
+function incrementCount() {
+  count.value++;
+  //console.log(count.value)
+}
+
+// function limparLista() {
+//   courses = [];
+// }
+
+function addNovoCurso() {
+  
+  if(newCourse.title != '') {
+    courses.push({title: newCourse.title});
+    newCourse.title = '';
+  }
+
+}
+
+</script>
+
+<template>
+  <!-- no template não precisa add count.value quando cria a variavel como ref somente passa  o valor -->
+  <div>
+    Olá Vite
+  </div>
+  <h2>Contador:</h2>
+  <h2>{{ count }}</h2>
+  <button @click="incrementCount()"> Incrementar + </button>
+
+  <br>
+
+  <div>
+    <ul v-if="courses.length > 0">
+      <li v-for="course in courses" :key="course.title">
+       {{ course.title }}
+      </li>
+    </ul>
+  </div>
+
+  <div>
+    <input type="text" v-model="newCourse.title">
+    <button style="margin-left: 16px;" @click="addNovoCurso()">Adicionar curso</button>
+  </div>
+
+  <!-- <div>
+    <button style="margin-left: 16px;" @click="limparLista()">Excluir Lista</button>
+  </div> -->
+
+</template>
+
+<style scoped>
+
+</style>
